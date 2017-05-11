d7c0180a3caa484585b89bb0bd0047ea257c33c9
keck <github@perlhack.com>
2017-05-11 17:04:56 -0400

    Add falcon to CI

--- circle.sh
+++ circle.sh
@@ -15,7 +15,7 @@ bundle check --path=vendor/bundle || bundle install --path=vendor/bundle --jobs=
 
 sudo docker build --rm=false -t $DOCKER_IMAGE .
 
-[[ $CIRCLE_BRANCH =~ ^(release\/[^\/.]*|eagle|testing|staging|master)$ ]] || exit 0
+[[ $CIRCLE_BRANCH =~ ^(falcon|eagle)$ ]] || exit 0
 $GCLOUD_BIN --quiet components update --version 144.0.0
 $GCLOUD_BIN --quiet components update --version 144.0.0 kubectl
 echo $GCLOUD_JSON | base64 --decode -i > gcloud.json
@@ -24,7 +24,7 @@ $GCLOUD_BIN config set project $GCLOUD_PROJECT
 
 $GCLOUD_BIN docker -- push $DOCKER_IMAGE
 
-[[ $CIRCLE_BRANCH =~ ^(eagle|testing|staging|master)$ ]] || exit 0
+[[ $CIRCLE_BRANCH =~ ^(falcon|eagle)$ ]] || exit 0
 $GCLOUD_BIN config set container/use_client_certificate True
 $GCLOUD_BIN config set compute/zone $GCLOUD_ZONE
 $GCLOUD_BIN config set container/cluster $CIRCLE_BRANCH
